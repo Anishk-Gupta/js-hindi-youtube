bbca8e023f9af41d2ce90b65894273b953c9edd5
Anishk-Gupta <anishkgupta994@gmail.com>
2026-01-21 18:26:57 +0000

    coversion of data types

---
+++ 01_basics/03_coversionOperator.js
@@ -0,0 +1,38 @@
+// let score = "33abc"
+// console.log(score)
+// console.log(typeof score)
+// let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+// let isLoggedIn = "anishk";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 => true , 0=> false , "" => false , "anishk" => true
+// let number = 33
+// let str = String(number)
+// console.log(typeof str);
+// console.log(str);
+
+// operation
+
+
+let str1 = "anishk"
+let str2 = "gupta"
+let str3 = str1 + str2 + " good evening"
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2");
+console.log("1+2");
+console.log("1"+2+2);
+console.log(1+2+"2")
+let num = 100
+console.log(num++); // firstly print num then increment
+console.log(num)
+console.log(++num); // firstly increment then print
+
+
+
+
