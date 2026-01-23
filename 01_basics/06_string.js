78666b893913dba56b600024af320abf773bc2c3
Anishk-Gupta <anishkgupta994@gmail.com>
2026-01-23 10:46:23 +0000

    commit

---
+++ 01_basics/06_string.js
@@ -0,0 +1,34 @@
+let str = "anishk"
+//console.log(str[0])
+let str1 = "gupta"
+let num = 50
+// let str3 = str+str1+50+"hello"
+// console.log(str3);
+// console.log(`hello ${str} ${str1} ${num}`)
+let game = new String("subwaySurf")
+// console.log(game)
+// console.log(game.toUpperCase());
+// console.log(game.length);
+// console.log(game.charAt(2))
+// console.log(game.indexOf('b'));
+// const newGame = game.substring(0,6)
+// const anotherString = game.slice(-10,-5)
+// console.log(anotherString)
+// console.log(newGame);
+// const newString = "   hitesh  10  "
+// console.log(newString);
+// console.log(newString.trim())
+const url = "https://www.google//.20%.com"
+console.log(url.replace('20%','-'))
+console.log(url.includes('20%'));
+console.log(url.split('//'))
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/07_numberMaths.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score);
+// const value = new Number(100)
+// console.log(value);
+// console.log(value.toString());
+// console.log(value.toFixed(2));
+// const other_number = 123.8966
+// console.log(other_number.toPrecision(3));
+
+// const hundereds = 1000000;
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// ************************maths**********************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min) // yaad rkhna hai 
